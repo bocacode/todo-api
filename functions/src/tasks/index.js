f09839694ef64ad2b0f5dc5457ea47a67010edad
jaRue1 <jaruejohnson@outlook.com>
2021-04-22 11:34:38 -0400

    Added the Update Task Feature

--- functions/src/tasks/index.js
+++ functions/src/tasks/index.js
@@ -35,3 +35,11 @@ exports.createTask = (req, res) => {
     .then(() => this.getTasks(req, res))
     .catch(err => res.status(500).send('Error creating task: ' + err.message))
 }
+exports.updateTask = (req, res) => {
+  connectToFirestore()
+  const updateTask = req.body
+  db.collection('tasks').doc(req.params.taskId).update(updateTask)
+    .then(() => this.getTasks(req, res))
+    .catch(err => res.status(500).send('Error creating task: ' + err.message))
+}
+
