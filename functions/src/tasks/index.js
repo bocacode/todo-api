cdedda789d33c6fa61f0da9b6757be3f03633d87
jaRue1 <jaruejohnson@outlook.com>
2021-04-22 13:10:11 -0400

    removed pakage-lock and change variable name

--- functions/src/tasks/index.js
+++ functions/src/tasks/index.js
@@ -37,8 +37,8 @@ exports.createTask = (req, res) => {
 }
 exports.updateTask = (req, res) => {
   connectToFirestore()
-  const updateTask = req.body
-  db.collection('tasks').doc(req.params.taskId).update(updateTask)
+  const taskUpdate = req.body
+  db.collection('tasks').doc(req.params.taskId).update(taskUpdate)
     .then(() => this.getTasks(req, res))
     .catch(err => res.status(500).send('Error creating task: ' + err.message))
 }
